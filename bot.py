e084cd6f24d6eecc3a52ebb80aabadea040db223
mthsfurio <66267841+mthsfurio@users.noreply.github.com>
2022-05-08 17:40:33 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,20 +45,19 @@ async def sender_bH(event):
                 i,
                 event.message
             )
-            @BotzHubUser.on(events.MessageEdited(incoming=True, chats=FROM))
-                async def handler(event):
-                    for e in TO:
-                            try:
-                                await BotzHubUser.send_message(
-                                    e,
-                                    event.message
-                                )
-                            except Exception as e:
-                                print(e)
         except Exception as e:
             print(e)
 
-
+@BotzHubUser.on(events.MessageEdited(incoming=True, chats=FROM))
+async def handler(event):
+    for e in TO:
+            try:
+                await BotzHubUser.send_message(
+                    e,
+                    event.message
+                )
+            except Exception as e:
+                print(e)
 
 
 print("Bot has started.")
